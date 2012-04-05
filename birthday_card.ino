f07d406aee561a0957eefcddbf22b66254bdf313
Brian Luft <brian@unbracketed.com>
2012-04-04 22:45:28 -0700

    rest of unsaved buffer :)

--- birthday_card.ino
+++ birthday_card.ino
@@ -1,10 +1,12 @@
 
 /* Birthday Card
-
-ShiftPWM RGB LED ring with tone speaker
-Each LED is assigned to a tone
-Rainbow effect at end of song
-*/
+ 
+ ShiftPWM RGB LED ring with tone speaker
+ Each LED is assigned to a tone
+ Rainbow effect at end of song
+ 
+ Speaker is on Pin 7
+ */
 
 #include <SPI.h>
 #include "hsv2rgb.h"
@@ -45,10 +47,51 @@ void setup()   {
 
 
   ShiftPWM.SetAmountOfRegisters(numRegisters);
-  ShiftPWM.Start(pwmFrequency,maxBrightness);  
-  
-  ShiftPWM.SetAll(0);
+
+  // iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second 
+    // divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000/noteDurations[thisNote];
+    tone(7, melody[thisNote],noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(7);
+
+    ShiftPWM.Start(pwmFrequency,maxBrightness);  
+
+    ShiftPWM.SetAll(0);
+
+    rgbLedRainbow(numRegisters*8/3, 5, 3, maxBrightness, numRegisters*8/3); // Fast, over all LED's
+  }
 }
 
 void loop() {
 }
+
+void rgbLedRainbow(int numRGBLeds, int delayVal, int numCycles, int maxBrightness, int rainbowWidth){
+  // Displays a rainbow spread over all LED's, which shifts in hue.
+  int hue, sat, val; 
+  unsigned char red, green, blue;
+
+  ShiftPWM.SetAll(0);
+  for(int cycle=0;cycle<numCycles;cycle++){ // shift the raibom numCycles times
+    for(int colorshift=0;colorshift<360;colorshift++){ // Shift over full color range (like the hue slider in photoshop)
+      for(int led=0;led<numRGBLeds;led++){ // loop over all LED's
+        hue = ((led)*360/(rainbowWidth-1)+colorshift)%360; // Set hue from 0 to 360 from first to last led and shift the hue
+        sat = 255;
+        val = 255;
+        hsv2rgb(hue, sat, val, &red, &green, &blue, maxBrightness); // convert hsv to rgb values
+        ShiftPWM.SetGroupOf3(led, red, green, blue); // write rgb values
+      }
+      delay(delayVal);
+    } 
+  }  
+}
+
